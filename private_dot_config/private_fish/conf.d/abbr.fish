bcb9278473856b75e307615c69a11c8044963d83
prinzpiuz <prinzpiuz@gmail.com>
2025-04-01 01:36:36 +0530

    Add .config/fish/conf.d/abbr.fish

---
+++ private_dot_config/private_fish/conf.d/abbr.fish
@@ -0,0 +1,80 @@
+#flutter & android
+abbr sdkmanager '/usr/lib/Android/cmdline-tools/latest/bin/sdkmanager'
+abbr android 'flutter emulator --launch pixel'
+
+#blutooth
+abbr bt 'bluetoothctl power on && bluetoothctl connect 80:C3:BA:4F:A9:94'
+abbr bt_off 'bluetoothctl power off'
+
+#postgresql
+abbr pg_start 'systemctl start postgresql'
+abbr pg_restart 'systemctl restart postgresql'
+abbr pg_stop 'systemctl stop postgresql'
+abbr pg_admin 'sudo su - postgres'
+
+#mysql
+abbr mstart 'systemctl start mysql'
+abbr mstop 'systemctl stop mysql'
+abbr mstatus 'systemctl status mysql'
+abbr mrestart 'systemctl restart mysql'
+
+#redis
+abbr rstart 'systemctl start redis-server'
+abbr rstop 'systemctl stop redis-server'
+abbr rstatus 'systemctl status redis-server'
+abbr rrestart 'systemctl restart redis-server'
+
+#debian
+abbr update 'sudo apt update && sudo apt upgrade -y'
+abbr clean 'sudo apt autoremove && sudo apt autoclean'
+abbr install 'sudo apt install'
+abbr remove 'sudo apt remove'
+abbr search 'apt search'
+
+#general & linux
+abbr p 'ping -i 1 -c 5 -w 30 -q 8.8.8.8'
+abbr nd 'sudo chattr +i'
+abbr un 'sudo chattr -i'
+abbr ll 'eza --long --all  --header --git --icons --color=always --group-directories-first'
+abbr list_with_size 'eza --long --all  --header --git --icons --total-size --color=always --group-directories-first'
+
+#kitty
+abbr pic 'kitty +kitten icat'
+abbr s 'kitty +kitten ssh'
+abbr d 'kitty +kitten diff'
+abbr themes 'kitty +kitten themes'
+abbr update_kitty 'curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin'
+
+#vpn
+abbr vpn_session 'openvpn3 sessions-list | grep Path | awk '{ print \$2 }''
+abbr vpn_disconnect 'openvpn3 session-manage --disconnect --session-path'
+abbr ms 'mullvad status'
+abbr mc 'mullvad connect'
+abbr md 'mullvad disconnect'
+
+#fish
+abbr reload 'source ~/.config/fish/config.fish'
+abbr tc 'tide configure'
+
+#git
+abbr gca 'git commit --amend -m'
+abbr pcr 'pre-commit run --all-files'
+
+#ledger
+abbr balance 'ledger -f $LEDGER balance'
+abbr ledger_summary 'paisa serve --config $PAISA_CONFIG'
+
+#python
+abbr run 'python manage.py runserver || flutter run'
+abbr Ipython3 'python3 -m IPython'
+
+#restic
+abbr restic_repo_unlock '$RESTIC_BASE_COMMAND unlock'
+abbr restic_repo_ls '$RESTIC_BASE_COMMAND snapshots'
+abbr restic_list '$RESTIC_BASE_COMMAND list'
+abbr restic_stats '$RESTIC_BASE_COMMAND stats'
+abbr rp_dry_run '$DELETE_UNWANTED_FILES || $RESTICPROFILE_BASE_COMMAND backup --dry-run'
+abbr rp_run '$DELETE_UNWANTED_FILES || $RESTICPROFILE_BASE_COMMAND backup'
+abbr rp_schedule 'restic_repo_unlock && $DELETE_UNWANTED_FILES || $RESTICPROFILE_BASE_COMMAND schedule'
+abbr rp_remove_schedule 'restic_repo_unlock && $DELETE_UNWANTED_FILES || $RESTICPROFILE_BASE_COMMAND unschedule'
+abbr rp_schedule_status '$RESTICPROFILE_BASE_COMMAND status'
