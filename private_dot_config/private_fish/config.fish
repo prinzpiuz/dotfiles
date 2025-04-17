75ee3801bd9f82baf78c5dd535f98e7f3116a444
prinzpiuz <prinzpiuz@gmail.com>
2025-04-17 14:31:44 +0530

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting ""
 source $HOME/.config/fish/conf.d/abbr.fish
 source $HOME/.config/fish/conf.d/env.fish
 source $HOME/.config/fish/functions/my_functions.fish
+source $HOME/.cargo/env.fish
 
 
 auto_alias_project_directories
