ab337609624852cfcea6b1f4a556c523701f8fcf
prinzpiuz <prinzpiuz@gmail.com>
2026-04-03 22:14:19 +0530

    Update .config/nvim/lua/plugins/themes.lua

--- private_dot_config/nvim/lua/plugins/themes.lua
+++ private_dot_config/nvim/lua/plugins/themes.lua
@@ -2,7 +2,7 @@ return {
     {
         "bluz71/vim-moonfly-colors",
         name = "moonfly",
-        lazy = false,
+        lazy = true,
         priority = 1000,
         config = function()
             -- Moonfly options (set before colorscheme loads)
@@ -11,7 +11,44 @@ return {
             vim.g.moonflyWinSeparator = 2        -- Thicker window separators
             vim.g.moonflyVirtualTextColor = true -- Colored virtual text for diagnostics
 
-            vim.cmd.colorscheme("moonfly")
+            -- vim.cmd.colorscheme("moonfly")
+        end,
+    },
+    {
+        "olimorris/onedarkpro.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("onedarkpro").setup({
+                colors = {},
+                highlights = {},
+                styles = {
+                    types = "NONE",
+                    methods = "NONE",
+                    numbers = "NONE",
+                    strings = "NONE",
+                    comments = "italic",
+                    keywords = "bold",
+                    constants = "NONE",
+                    functions = "italic",
+                    operators = "NONE",
+                    variables = "NONE",
+                    parameters = "NONE",
+                    conditionals = "italic",
+                    virtual_text = "NONE",
+                },
+                options = {
+                    bold = true,
+                    italic = true,
+                    underline = true,
+                    undercurl = true,
+                    cursorline = true,
+                    transparency = false,
+                    terminal_colors = true,
+                    highlight_inactive_windows = false,
+                },
+            })
+            vim.cmd.colorscheme("onedark")
         end,
     },
 }
