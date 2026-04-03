6cf8821419b11f27d4684d2978bf1375411b3d26
prinzpiuz <prinzpiuz@gmail.com>
2026-04-03 21:54:59 +0530

    Update .config/nvim/lua/plugins/themes.lua

--- private_dot_config/nvim/lua/plugins/themes.lua
+++ private_dot_config/nvim/lua/plugins/themes.lua
@@ -1,3 +1,17 @@
 return {
-    { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+    {
+        "bluz71/vim-moonfly-colors",
+        name = "moonfly",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Moonfly options (set before colorscheme loads)
+            vim.g.moonflyItalics = true
+            vim.g.moonflyNormalFloat = true
+            vim.g.moonflyWinSeparator = 2        -- Thicker window separators
+            vim.g.moonflyVirtualTextColor = true -- Colored virtual text for diagnostics
+
+            vim.cmd.colorscheme("moonfly")
+        end,
+    },
 }
