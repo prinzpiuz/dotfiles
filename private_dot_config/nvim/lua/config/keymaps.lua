bfdf19ef478ff67fc5c2738512ecbd4fa5d4187f
prinzpiuz <prinzpiuz@gmail.com>
2026-04-09 14:36:00 +0530

    Update .config/nvim/lua/config/keymaps.lua

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -40,7 +40,11 @@ vim.diagnostic.config {
     virtual_lines = false, -- Text shows up underneath the line, with virtual lines
 
     -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
-    jump = { float = true },
+    jump = {
+        on_jump = function(diagnostic)
+            vim.diagnostic.open_float()
+        end,
+    },
 }
 map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
