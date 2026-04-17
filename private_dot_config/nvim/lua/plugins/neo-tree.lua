afdcc377feec785eb5ce4e13190727727573ba1c
prinzpiuz <prinzpiuz@gmail.com>
2026-04-17 16:43:10 +0530

    Add .config/nvim/.claude/settings.local.json Update .config/nvim/lazy-lock.json Add .config/nvim/lua/plugins/neo-tree.lua Add .config/nvim/lua/plugins/project-root.lua Add .config/nvim/lua/plugins/project.lua

---
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,26 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            window = {
+                position = "left",
+                width = 30,
+            },
+            filesystem = {
+                follow_current_file = { enabled = true },
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                },
+            },
+        })
+        vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle file tree" })
+    end,
+}
