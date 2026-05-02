e9485362b286f358a0027847cd8318ce20ee3806
prinzpiuz <prinzpiuz@gmail.com>
2026-05-02 23:38:19 +0530

    Update .config/nvim/lua/plugins/debug.lua

--- private_dot_config/nvim/lua/plugins/debug.lua
+++ private_dot_config/nvim/lua/plugins/debug.lua
@@ -1,5 +1,6 @@
 return {
     'mfussenegger/nvim-dap',
+    lazy = false,
     dependencies = {
         'rcarriga/nvim-dap-ui',
         'nvim-neotest/nvim-nio',
@@ -9,18 +10,16 @@ return {
         'leoluz/nvim-dap-go',
         'mfussenegger/nvim-dap-python',
     },
-    keys = {
-        { '<F5>',      function() require('dap').continue() end,                                            desc = 'Debug: Start/Continue' },
-        { '<F1>',      function() require('dap').step_into() end,                                           desc = 'Debug: Step Into' },
-        { '<F2>',      function() require('dap').step_over() end,                                           desc = 'Debug: Step Over' },
-        { '<F3>',      function() require('dap').step_out() end,                                            desc = 'Debug: Step Out' },
-        { '<leader>b', function() require('dap').toggle_breakpoint() end,                                   desc = 'Debug: Toggle Breakpoint' },
-        { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
-        { '<F7>',      function() require('dapui').toggle() end,                                            desc = 'Debug: See last session result.' },
-    },
-    config = function()
+        config = function()
         local dap = require 'dap'
         local dapui = require 'dapui'
+        vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+        vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+        vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+        vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+        vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+        vim.keymap.set('n', '<leader>B', function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, { desc = 'Debug: Set Breakpoint' })
+        vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: Toggle UI' })
 
         require('mason-nvim-dap').setup {
             automatic_installation = true,
