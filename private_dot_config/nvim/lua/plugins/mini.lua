eebc1933a3529f1e46b712745187a384bf108630
prinzpiuz <prinzpiuz@gmail.com>
2026-05-03 16:27:15 +0530

    Update .config/nvim/lua/plugins/mini.lua

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -15,10 +15,32 @@ return {
         require('mini.surround').setup()
 
         -- Simple and easy statusline.
-        local statusline = require 'mini.statusline'
-        statusline.setup { use_icons = vim.g.have_nerd_font }
+        local statusline = require('mini.statusline')
+        statusline.setup({
+        use_icons = vim.g.have_nerd_font,
+        content = {
+            active = function()
+                local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+                local git = statusline.section_git({ trunc_width = 40 })
+                local diff = statusline.section_diff({ trunc_width = 75 })
+                local diagnostics = statusline.section_diagnostics({ trunc_width = 75 })
+                local lsp = statusline.section_lsp({ trunc_width = 75 })
+                local filename = statusline.section_filename({ trunc_width = 140 })
+                local fileinfo = statusline.section_fileinfo({ trunc_width = 120 })
+                local location = statusline.section_location({ trunc_width = 75 })
+                local search = statusline.section_searchcount({ trunc_width = 75 })
 
-        ---@diagnostic disable-next-line: duplicate-set-field
-        statusline.section_location = function() return '%2l:%-2v' end
+                return statusline.combine_groups({
+                    { hl = mode_hl,                 strings = { mode } },
+                    { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+                    '%<',
+                    { hl = 'MiniStatuslineFilename', strings = { filename } },
+                    '%=',
+                    { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                    { hl = mode_hl,                  strings = { search, location } },
+                })
+            end,
+        },
+})
     end,
 }
