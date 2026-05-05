f594a2f605807b4f65bcc9c009f6162c40613d1f
prinzpiuz <prinzpiuz@gmail.com>
2026-05-05 16:00:11 +0530

    Add .config/nvim/lua/plugins/lualine.lua

---
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,31 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "16color",
+				component_separators = { left = "|", right = "|" },
+				section_separators = { left = "", right = "" },
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location", "searchcount" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
+}
