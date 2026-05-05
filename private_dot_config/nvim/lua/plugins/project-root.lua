01d0119d570ed3f1073103c490da4567a8ec9c00
prinzpiuz <prinzpiuz@gmail.com>
2026-05-05 14:05:34 +0530

    Remove .config/nvim/lua/plugins/project-root.lua Remove .config/nvim/lua/plugins/project.lua

--- private_dot_config/nvim/lua/plugins/project-root.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'ahmedkhalf/project.nvim',
-    config = function()
-        require("project_nvim").setup {
-            detection_methods = { "lsp", "pattern" },
-            patterns = { ".git", "go.mod", "package.json", "pyproject.toml", "uv.lock" },
-        }
-    end
-}
