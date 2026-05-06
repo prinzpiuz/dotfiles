e08a4034ff416d54e83217af0814a155da4209bd
prinzpiuz <prinzpiuz@gmail.com>
2026-05-06 16:26:31 +0530

    Update .config/nvim/lua/plugins/flutter-tools.lua

--- private_dot_config/nvim/lua/plugins/flutter-tools.lua
+++ private_dot_config/nvim/lua/plugins/flutter-tools.lua
@@ -1,11 +1,16 @@
 return {
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- Optional: for better UI
-    },
-    config = function()
-        require("flutter-tools").setup {}
-    end,
+	"akinsho/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim",
+	},
+	config = function()
+		require("flutter-tools").setup({
+			debugger = {
+				enabled = true,
+				run_via_dap = true,
+			},
+		})
+	end,
 }
