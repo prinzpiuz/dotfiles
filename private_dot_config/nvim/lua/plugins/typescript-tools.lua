060eac831e936beb3133107d1828c20a5183882c
prinzpiuz <prinzpiuz@gmail.com>
2026-05-07 16:07:40 +0530

    Update .config/nvim/lua/plugins/typescript-tools.lua

--- private_dot_config/nvim/lua/plugins/typescript-tools.lua
+++ private_dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -1,6 +1,6 @@
 -- TS-JS LSP
 return {
-    'pmizio/typescript-tools.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+	"pmizio/typescript-tools.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+	opts = {},
 }
