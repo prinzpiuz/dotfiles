b8eea5c283c684bffde13048e9fb493cdd4f2c77
prinzpiuz <prinzpiuz@gmail.com>
2026-07-31 18:29:55 +0530

    Update .config/nvim/lua/plugins/markdown-preview.lua

--- private_dot_config/nvim/lua/plugins/markdown-preview.lua
+++ private_dot_config/nvim/lua/plugins/markdown-preview.lua
@@ -2,9 +2,7 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
-	end,
+	build = "cd app && npm install",
 	config = function()
 		vim.g.mkdp_auto_close = 1
 		vim.g.mkdp_open_to_the_world = 0
