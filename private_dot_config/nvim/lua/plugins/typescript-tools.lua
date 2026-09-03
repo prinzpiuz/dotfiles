8c548ba20f97391c90be2eb3ba163afc6f58320a
prinzpiuz <prinzpiuz@gmail.com>
2026-09-03 16:46:57 +0530

    Update .config/nvim/lua/plugins/typescript-tools.lua

--- private_dot_config/nvim/lua/plugins/typescript-tools.lua
+++ private_dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -1,12 +1,14 @@
--- TS-JS LSP
 return {
 	"pmizio/typescript-tools.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
 	config = function()
 		local util = require("lspconfig.util")
 		require("typescript-tools").setup({
-			root_dir = util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git"),
+			root_dir = util.root_pattern("tsconfig.json", "package.json", ".git"),
+			settings = {
+				expose_as_code_action = "all",
+			},
 		})
 	end,
 }
