0da9be24ecac62009fad174abed0599410c77084
prinzpiuz <prinzpiuz@gmail.com>
2026-09-08 11:11:38 +0530

    Update .config/nvim/lua/plugins/which-key.lua

--- private_dot_config/nvim/lua/plugins/which-key.lua
+++ private_dot_config/nvim/lua/plugins/which-key.lua
@@ -1,4 +1,5 @@
 -- Show pending keybinds
+-- Show pending keybinds
 return {
 	"folke/which-key.nvim",
 	event = "VimEnter",
@@ -9,14 +10,47 @@ return {
 		delay = 0,
 		icons = { mappings = vim.g.have_nerd_font },
 		spec = {
+			-- Search group (Telescope)
 			{ "<leader>s", group = "[S]earch", mode = { "n", "v" } },
-			{ "<leader>t", group = "[T]oggle" },
+
+			-- Toggle group
+			{ "<leader>t", group = "[T]oggle / [T]erminal" },
+
+			-- Git Hunk group (Gitsigns)
 			{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+
+			-- LSP actions
 			{ "gr", group = "LSP Actions", mode = { "n" } },
+
+			-- Debug group (DAP)
+			{ "<leader>d", group = "[D]ebug" },
+
+			-- AI group (CodeCompanion)
+			{ "<leader>a", group = "[A]I", mode = { "n", "v" } },
+
+			-- Flutter group
 			{ "<leader>F", group = "[F]lutter" },
+
+			-- Database group (DBee)
 			{ "<leader>D", group = "[D]atabase" },
-			{ "<leader>a", group = "[A]I" },
+
+			-- Yank path group
 			{ "<leader>y", group = "[Y]ank path" },
+
+			-- Git group (Diffview)
+			{ "<leader>g", group = "[G]it" },
+
+			-- Trouble/diagnostics group
+			{ "<leader>x", group = "Trouble / Diagnostics" },
+
+			-- Code group (format, diagnostics copy)
+			{ "<leader>c", group = "[C]ode" },
+
+			-- Markdown group
+			{ "<leader>m", group = "[M]arkdown" },
+
+			-- Remote group (remote-sshfs)
+			{ "<leader>r", group = "[R]emote" },
 		},
 	},
 }
