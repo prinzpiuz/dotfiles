5b923028aef21c897ef78c7bbf1675bcf61e6079
prinzpiuz <prinzpiuz@gmail.com>
2026-09-18 10:02:17 +0530

    Update .config/nvim/lua/plugins/code_companion.lua

--- private_dot_config/nvim/lua/plugins/code_companion.lua
+++ private_dot_config/nvim/lua/plugins/code_companion.lua
@@ -26,6 +26,10 @@ return {
 							commands = {
 								default = { vim.env.HOME .. "/.volta/bin/claude-agent-acp" },
 							},
+							env = {
+								ANTHROPIC_API_KEY = "",
+								ANTHROPIC_AUTH_TOKEN = "",
+							},
 						})
 					end,
 				},
