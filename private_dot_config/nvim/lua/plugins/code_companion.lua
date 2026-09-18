d73f353f7f0fbb998cc5e66c3e47893e224141ae
prinzpiuz <prinzpiuz@gmail.com>
2026-09-18 10:56:27 +0530

    Update .config/nvim/lua/plugins/code_companion.lua

--- private_dot_config/nvim/lua/plugins/code_companion.lua
+++ private_dot_config/nvim/lua/plugins/code_companion.lua
@@ -17,7 +17,7 @@ return {
 					codex = function()
 						return require("codecompanion.adapters").extend("codex", {
 							defaults = {
-								auth_method = "chatgpt",
+								auth_method = "chat-gpt",
 							},
 						})
 					end,
