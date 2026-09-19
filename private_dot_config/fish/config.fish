016a36f89528faace7b1261ef37bc5d05a966b7a
prinzpiuz <prinzpiuz@gmail.com>
2026-09-19 13:24:39 +0530

    Update .config/fish/config.fish

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -3,6 +3,9 @@ source $HOME/.config/fish/conf.d/abbr.fish
 source $HOME/.config/fish/conf.d/env.fish
 source $HOME/.config/fish/functions/my_functions.fish
 
+if test -f ~/.config/fish/secrets.fish
+    source ~/.config/fish/secrets.fish
+end
 
 auto_alias_project_directories
 fish_vi_key_bindings
